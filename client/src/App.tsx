82c364fb57d895fa80a3c8475b09af2b4f4988a1
stuartthomson <stuartwthomson@live.co.uk>
2019-06-26 15:52:38 +0000

    Stupid endpoint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+    fetch ('https://8080-dd65346a-508b-4d7d-844d-08899a696944.ws-eu0.gitpod.io/', {mode: 'no-cors'}).then( results => {
+        console.log(results);
+    });
   return (
     <div className="App">
       <header className="App-header">
@@ -22,5 +25,6 @@ const App: React.FC = () => {
     </div>
   );
 }
+
 
 export default App;
